51bfd37829946c4b45b58cb498f1e53c27904550
c.martinezc1 <c.martinezc1@ISC422.sis.virtual.uniandes.edu.co>
2017-09-11 15:25:55 -0500

    Cliente error2

---
+++ huecota-back/src/test/java/co/edu/uniandes/nocompila/huecota/persistence/ClientePersistenceTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.nocompila.huecota.persistence;
+
+import co.edu.uniandes.nocompila.huecota.entities.ClienteEntity;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author c.martinezc1
+ */
+@RunWith(Arquillian.class)
+public class ClientePersistenceTest {
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ClienteEntity.class.getPackage())
+                .addPackage(ClientePersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    
+}
