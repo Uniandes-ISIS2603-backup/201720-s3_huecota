29b55af4ccc3db5441d1a62fa800c4d36d7b40ea
MIGUEL ANGEL PUENTES RAMIREZ <ma.puentes@sis.virtual.uniandes.edu.co>
2017-09-10 20:55:56 -0500

    Correccion en la documentacion de AccidentePersistence @ma.puentes

--- huecota-back/src/main/java/co/edu/uniandes/nocompila/huecota/persistence/AccidentePersistence.java
+++ huecota-back/src/main/java/co/edu/uniandes/nocompila/huecota/persistence/AccidentePersistence.java
@@ -35,26 +35,23 @@ public class AccidentePersistence
     public AccidenteEntity create(AccidenteEntity entity)
 	{
         LOGGER.info("Creando un accidente nuevo");
-        /* Note que hacemos uso de un m�todo propio de EntityManager para persistir la Hueco en la base de datos.
-        Es similar a "INSERT INTO table_codigo (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);" en SQL.
-         */
         em.persist(entity);
         LOGGER.info("Creando un accidente nuevo");
         return entity;
     }
 
     /**
-     * Actualiza un huecota.
+     * Actualiza un accidente.
      *
-     * @param entity: la Hueco que viene con los nuevos cambios. Por ejemplo
-     * el codigo pudo cambiar. En ese caso, se haria uso del m�todo update.
-     * @return un huecota con los cambios aplicados.
+     * @param entity: el accidente que viene con los nuevos cambios. Por ejemplo
+     * el codigo pudo cambiar. En ese caso, se haria uso del m�todo update.
+     * @return un accidente con los cambios aplicados.
      */
     public AccidenteEntity update(AccidenteEntity entity)
 	{
-        LOGGER.log(Level.INFO, "Actualizando Hueco con id={0}", entity.getId());
+        LOGGER.log(Level.INFO, "Actualizando accidente con id={0}", entity.getId());
         /* Note que hacemos uso de un método propio del EntityManager llamado merge() que recibe como argumento
-        la Hueco con los cambios, esto es similar a 
+        el accidente con los cambios, esto es similar a 
         "UPDATE table_codigo SET column1 = value1, column2 = value2, ... WHERE condition;" en SQL.
          */
         return em.merge(entity);
@@ -62,24 +59,24 @@ public class AccidentePersistence
 
     /**
      *
-     * Borra un huecota de la base de datos recibiendo como argumento el id
-     * de la Hueco
+     * Borra un accidente de la base de datos recibiendo como argumento el id
+     * de el accidente
      *
-     * @param id: id correspondiente a la Hueco a borrar.
+     * @param id: id correspondiente al accidente a borrar.
      */
     public void delete(Long id)
 	{
         LOGGER.log(Level.INFO, "Borrando accidente con id={0}", id);
-        // Se hace uso de mismo m�todo que esta explicado en public HuecoEntity find(Long id) para obtener la Hueco a borrar.
+        // Se hace uso de mismo m�todo que esta explicado en public AccidenteEntity find(Long id) para obtener el accidente a borrar.
         AccidenteEntity entity = em.find(AccidenteEntity.class, id);
         /* Note que una vez obtenido el objeto desde la base de datos llamado "entity", volvemos hacer uso de un método propio del
          EntityManager para eliminar de la base de datos el objeto que encontramos y queremos borrar.
-         Es similar a "delete from HuecoEntity where id=id;" - "DELETE FROM table_codigo WHERE condition;" en SQL.*/
+         Es similar a "delete from AccidenteEntity where id=id;" - "DELETE FROM table_codigo WHERE condition;" en SQL.*/
         em.remove(entity);
     }
 
     /**
-     * Busca si hay algun huecota con el id que se envía de argumento
+     * Busca si hay algun accidente con el id que se envía de argumento
      *
      * @param id: id correspondiente al accidente buscado.
      * @return un accidente.
@@ -89,24 +86,24 @@ public class AccidentePersistence
         LOGGER.log(Level.INFO, "Consultando accidente con id={0}", id);
         /* Note que se hace uso del metodo "find" propio del EntityManager, el cual recibe como argumento 
         el tipo de la clase y el objeto que nos hara el filtro en la base de datos en este caso el "id"
-        Suponga que es algo similar a "select * from HuecoEntity where id=id;" - "SELECT * FROM table_codigo WHERE condition;" en SQL.
+        Suponga que es algo similar a "select * from AccidenteEntity where id=id;" - "SELECT * FROM table_codigo WHERE condition;" en SQL.
          */
         return em.find(AccidenteEntity.class, id);
     }
 
     /**
-     * Devuelve todas las Huecoes de la base de datos.
+     * Devuelve todas las accidentes de la base de datos.
      *
-     * @return una lista con todas las Huecoes que encuentre en la base de
-     * datos, "select u from HuecoEntity u" es como un "select * from
-     * HuecoEntity;" - "SELECT * FROM table_codigo" en SQL.
+     * @return una lista con todas los accidentes que encuentre en la base de
+     * datos, "select u from AccidenteEntity u" es como un "select * from
+     * AccidenteEntity;" - "SELECT * FROM table_codigo" en SQL.
      */
     public List<AccidenteEntity> findAll()
 	{
-        LOGGER.info("Consultando todas las Huecoes");
-        // Se crea un query para buscar todas las Huecoes en la base de datos.
+        LOGGER.info("Consultando todos los accidentes");
+        // Se crea un query para buscar todas los accidentes en la base de datos.
         TypedQuery query = em.createQuery("select u from AccidenteEntity u", AccidenteEntity.class);
-        // Note que en el query se hace uso del método getResultList() que obtiene una lista de Huecoes.
+        // Note que en el query se hace uso del método getResultList() que obtiene una lista de accidentes.
         return query.getResultList();
     }
 }
