78889cc494d10693c57549f098dabf7034590a99
jpr.arango10 <jpr.arango10@ISIS2603S3-0021.sis.virtual.uniandes.edu.co>
2017-09-11 14:47:59 -0500

    Persistencia de la clase Estado, no es la definitiva.

---
+++ huecota-back/src/main/java/co/edu/uniandes/nocompila/huecota/entities/EnProgresoEntity.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.nocompila.huecota.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author jpr.arango10
+ */
+public class EnProgresoEntity extends BaseEntity implements Serializable{
+    
+    /**
+     * Fecha de inicio de tapado del hueco.
+     */
+    private Date fechaDeInicio;
+
+    /**
+     * Retorna la fecha de inicio.
+     * @return fechaDeInicio
+     */
+    public Date getFechaDeInicio() {
+        return fechaDeInicio;
+    }
+
+    /**
+     * Fija una fecha de inicio del tapado del hueco.
+     * @param fechaDeInicio 
+     */
+    public void setFechaDeInicio(Date fechaDeInicio) {
+        this.fechaDeInicio = fechaDeInicio;
+    }
+}
---
+++ huecota-back/src/main/java/co/edu/uniandes/nocompila/huecota/persistence/CerradoPersistence.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.nocompila.huecota.persistence;
+
+import co.edu.uniandes.nocompila.huecota.entities.AbiertoEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author jpr.arango10
+ */
+@Stateless
+public class CerradoPersistence {
+    private static final Logger LOGGER = Logger.getLogger(AbiertoPersistence.class.getName());
+        
+    @PersistenceContext(unitName = "huecotaPU")
+    protected EntityManager em;
+    
+     /**
+     *
+     * Crea una entidad de estado con la base de datos.
+     * @param entity objeto accidente que se crea en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
+    public AbiertoEntity create(AbiertoEntity entity)
+	{
+        LOGGER.info("Creando un estado nuevo");
+        
+        em.persist(entity);
+        
+        LOGGER.info("Creando un estado nuevo");
+        
+        return entity;
+    }
+    
+     /**
+     * Actualiza un estado.
+     * @param entity el estado que viene con los nuevos cambios.
+     * @return  un estado con los cambios aplicados
+     */
+    public AbiertoEntity update(AbiertoEntity entity)
+    {
+        LOGGER.log(Level.INFO, "Actualizando estado con id={0}", entity.getId());
+        
+        return em.merge(entity);
+    }
+    
+    /**
+     * Elimina un estado de la base de datos recibiendo como argumento el id del estado.
+     * @param id: id correspondiente al estado a borrar.
+     */
+    public void delete(Long id)
+    {
+        LOGGER.log(Level.INFO, "Eliminando estado con id={0}", id);
+        
+        AbiertoEntity entity = em.find(AbiertoEntity.class, id);
+        
+        em.remove(entity);
+    }
+    
+     /**
+     * Busca si hay algun estado con el id que se envia de argumento.
+     * @param id id correspondiente con la calificacion buscada.
+     * @return un estado.
+     */
+    public AbiertoEntity find(Long id)
+    {
+        LOGGER.log(Level.INFO, "Consultando estado con id={0}", id);
+        
+        return em.find(AbiertoEntity.class, id);
+    }
+    
+    /**
+     * Devuelve todos los estados de la base de datos.
+     * @return una lista con todos los estados que encuentre en la base de datos,
+     */
+    public List<AbiertoEntity> findAll()
+    {
+        LOGGER.info("Consultando todos los estados abiertos");
+        
+        TypedQuery query = em.createNamedQuery("select u from AbiertoEntity u", AbiertoEntity.class);
+        
+        return query.getResultList();
+    }
+}
+
