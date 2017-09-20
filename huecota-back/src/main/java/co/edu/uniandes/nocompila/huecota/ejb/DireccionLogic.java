d7a95672367408e72f835b4fdf277c622c28132c
MIGUEL ANGEL PUENTES RAMIREZ <ma.puentes@sis.virtual.uniandes.edu.co>
2017-09-20 11:42:11 -0500

    Agregacion de la clase DireccionLogic

---
+++ huecota-back/src/main/java/co/edu/uniandes/nocompila/huecota/ejb/DireccionLogic.java
@@ -0,0 +1,56 @@
+
+package co.edu.uniandes.nocompila.huecota.ejb;
+
+import co.edu.uniandes.nocompila.huecota.entities.DireccionEntity;
+import co.edu.uniandes.nocompila.huecota.exceptions.BusinessLogicException;
+import co.edu.uniandes.nocompila.huecota.persistence.DireccionPersistence;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+
+/**
+ *
+ * @author ma.puentes
+ */
+@Stateless
+public class DireccionLogic
+{
+
+    private static final Logger LOGGER = Logger.getLogger(HuecoLogic.class.getName());
+
+    @Inject
+    private DireccionPersistence persistence; // Variable para acceder a la persistencia de la aplicaci�n. Es una inyecci�n de dependencias.
+
+    /**
+     *
+     * @param entity
+     * @return
+     * @throws BusinessLogicException
+     */
+    public DireccionEntity createDireccion(DireccionEntity entity) throws BusinessLogicException 
+	{
+        LOGGER.info("Inicia proceso de creaci�n de una direccion");
+        // Invoca la persistencia para crear la direccion
+        persistence.create(entity);
+        LOGGER.info("Termina proceso de creaci�n de una direccion");
+        return entity;
+    }
+
+    /**
+     * 
+     * Obtener todas las Direcciones existentes en la base de datos.
+     *
+     * @return una lista de Huecoes.
+     */
+    public List<DireccionEntity> getDirecciones()
+	{
+        LOGGER.info("Inicia proceso de consultar todos las direcciones");
+        // Note que, por medio de la inyecci�n de dependencias se llama al m�todo "findAll()" que se encuentra en la persistencia.
+        List<DireccionEntity> direcciones = persistence.findAll();
+        LOGGER.info("Termina proceso de consultar todas las direcciones");
+        return direcciones;
+    }
+}
+
