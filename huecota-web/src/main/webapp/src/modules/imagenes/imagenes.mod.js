39f7bec661daaa17488c21aa54e832e9d89141ad
LUIS ERNESTO VIANA CASTILLO <le.viana@sis.virtual.uniandes.edu.co>
2017-10-30 10:15:01 -0500

    imagenes front

---
+++ huecota-web/src/main/webapp/src/modules/imagenes/imagenes.mod.js
@@ -0,0 +1,31 @@
+(function (ng) {
+    var mod = ng.module("imagenModule" ['huecoModule' ,'ui.router']);
+    mod.constant("imagenesContext", "imagenes");
+    mod.constant("huecosContext" ,"apo/huecos");
+    
+    mod.config(['$stateProvider' ,'$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath= 'src/modules/imagenes';
+            $urlRouterProvider.otherwise("/imagenesList");
+            
+            $stateProvider.state('imagenes', {
+                url: '/imagenes',
+                abstract:true,
+                parent:'huecoDetail',
+                views: {
+                    childrenView: {
+                        templateUrl: basePath + 'imagenes.html'
+                    }
+                }
+            }).state('imagenesList' , {
+                url: '/list',
+                parent: 'reviews',
+                views: {
+                    listView: {
+                        templateUrl: basePath + 'imagenes.list.html',
+                        controller:'imagenesCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            });
+    }]);
+})(window.angular);
