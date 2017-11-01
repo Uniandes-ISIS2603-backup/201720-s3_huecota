ecf19c5b72a94de5fecc0b4ddecfc61c7af4b8ae
MIGUEL ANGEL PUENTES RAMIREZ <ma.puentes@sis.virtual.uniandes.edu.co>
2017-11-01 11:28:45 -0500

    MOdulos experimentales de Direccion

---
+++ huecota-web/src/main/webapp/src/modules/direcciones/direcciones.ctrl.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function (ng)
+{
+    var mod = ng.module("DireccionModule");
+    mod.constant("direccionesContext", "api/");
+    mod.controller('DireccionCtrl', ['$scope', '$http', 'direccionesContext', '$state',
+        function ($scope, $http, direccionesContext, $state) {
+            $http.get(direccionesContext).then(function (response) {
+                $scope.direccionesRecords = response.data;
+            });
+
+            if ($state.params.direccionId !== undefined) {
+                $http.get(direccionesContext + '/' + $state.params.direccionId).then(function (response) {
+                    $scope.puntosRecords = response.data.punto;
+                    $scope.currentDireccion = response.data;
+                });
+            }
+        }
+    ]);
+}
+)(angular);
+
