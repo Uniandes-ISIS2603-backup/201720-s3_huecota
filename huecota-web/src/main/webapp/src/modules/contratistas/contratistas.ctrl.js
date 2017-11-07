67a7b7fefcd802957113ee1d0238bfefebacd908
LUIS CARLOS GARAVITO ROMERO <lc.garavito@sis.virtual.uniandes.edu.co>
2017-11-07 16:43:31 -0500

    contratistas.list - funcionando

--- huecota-web/src/main/webapp/src/modules/contratistas/contratistas.ctrl.js
+++ huecota-web/src/main/webapp/src/modules/contratistas/contratistas.ctrl.js
@@ -1,37 +1,9 @@
 (function(ng) {
     var mod = ng.module("contratistaModule");
     mod.constant("contratistasContext", "api/contratistas");
-    mod.controller('contratistaCtrl', ['$scope', '$state', '$stateParams', '$http', 'contratistasContext',
-    function($scope, $state, $stateParams, $http, contratistasContext){
-        $scope.records={};
-        $http.get(contratistasContext).then(function(response){
-            $scope.records = response.data;
-        });
-        if($stateParams.contratistaId !== null && $stateParams.contratistaId !== undefined){
-            id = $stateParams.contratistaId;
-            $http.get(contratistasContext + "/" + id).then(function(response){
-                $scope.currentRecord = response.data;
+    mod.controller('contratistaCtrl', ['$scope', '$http', 'contratistasContext', function($scope, $http, contratistasContext){
+            $http.get(contratistasContext).then(function(response){
+                $scope.contratistasRecords = response.data;
             });
-        }else{
-            $scope.currentRecord={
-                id: undefined,
-                name: '',
-            };
-            $scope.alerts = [];
-        }
-        
-        this.saveRecord = function(id){
-            currentRecord = $scope.currentRecord;
-            if(id == null){
-                return $http.post(contratistasContext, currentRecord).then(function(){
-                    $state.go('contratistasList');
-                });
-            }else{
-                return $http.put(contratistasContext + "/" + currentRecord.id, currentRecord).then(function(){
-                    $state.go('contratistasList');
-                });
-            };
-        };
-        
     }]);
-});
+})(window.angular);
--- huecota-web/src/main/webapp/src/modules/contratistas/contratistas.mod.js
+++ huecota-web/src/main/webapp/src/modules/contratistas/contratistas.mod.js
@@ -2,33 +2,12 @@
     var mod = ng.module("contratistaModule", ['ui.router']);
     mod.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
             var basePath = 'src/modules/contratistas/';
-            $urlRouterProvider.otherwise("contratistasList");
+            $urlRouterProvider.otherwise("/contratistasList");
             $stateProvider.state('contratistasList', {
                 url: '/contratistas/list',
                 views: {
                     'mainView': {
                         templateUrl: basePath + 'contratistas.list.html',
-                        controller: 'contratistaCtrl',
-                        controllerAs: 'ctrl'
-                    }
-                }
-            }).state('contratistaCreate', {
-                url: '/contratistas/create',
-                views: {
-                    'mainView': {
-                        templateUrl: basePath + 'contratistas.create.html',
-                        controller: 'contratistaCtrl',
-                        controllerAs: 'ctrl'
-                    }
-                }
-            }).state('contratistaEdit', {
-                url: '/contratistas/:contratistaId',
-                param: {
-                    contratistaId: null
-                },
-                views: {
-                    'mainView': {
-                        templateUrl: basePath + 'contratista.create.html',
                         controller: 'contratistaCtrl',
                         controllerAs: 'ctrl'
                     }
