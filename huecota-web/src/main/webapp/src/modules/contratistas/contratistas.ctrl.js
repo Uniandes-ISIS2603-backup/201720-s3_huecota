5f99d845ea64d8821671bdbb2a07b96c2567b280
LUIS CARLOS GARAVITO ROMERO <lc.garavito@sis.virtual.uniandes.edu.co>
2017-11-08 02:20:53 -0500

    GET - contratistas/id/cuentascobro

--- huecota-web/src/main/webapp/src/modules/contratistas/contratistas.ctrl.js
+++ huecota-web/src/main/webapp/src/modules/contratistas/contratistas.ctrl.js
@@ -1,9 +1,15 @@
 (function(ng) {
     var mod = ng.module("contratistaModule");
     mod.constant("contratistasContext", "api/contratistas");
-    mod.controller('contratistaCtrl', ['$scope', '$http', 'contratistasContext', function($scope, $http, contratistasContext){
+    mod.controller('contratistaCtrl', ['$scope', '$http', 'contratistasContext', '$state', function($scope, $http, contratistasContext, $state){
             $http.get(contratistasContext).then(function(response){
                 $scope.contratistasRecords = response.data;
             });
+            if(($state.params.contratistaId !== null) && ($state.params.contratistaId !== undefined)){
+                $http.get(contratistasContext + '/' + $state.params.contratistaId).then(function(response){
+                    $scope.cuentasRecords = response.data.cuentasCobro;
+                    $scope.currentContratista = response.data;
+                });
+            }
     }]);
 })(window.angular);
--- huecota-web/src/main/webapp/src/modules/contratistas/contratistas.mod.js
+++ huecota-web/src/main/webapp/src/modules/contratistas/contratistas.mod.js
@@ -2,12 +2,40 @@
     var mod = ng.module("contratistaModule", ['ui.router']);
     mod.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
             var basePath = 'src/modules/contratistas/';
+            var basePathCuentas = 'src/modules/cuentascobro/';
             $urlRouterProvider.otherwise("/contratistasList");
-            $stateProvider.state('contratistasList', {
-                url: '/contratistas/list',
+            $stateProvider.state('contratistas', {
+                url: '/contratistas',
+                abstract: true,
                 views: {
                     'mainView': {
-                        templateUrl: basePath + 'contratistas.list.html',
+                        templateUrl: basePath + 'contratistas.html',
+                        controller: 'contratistaCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('contratistasList', {
+                url: '/list',
+                parent: 'contratistas',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'contratistas.list.html'
+                    }
+                }
+            }).state('contratistaDetail', {
+                url: '/{contratistaId:int}/detail',
+                parent: 'contratistas',
+                param: {
+                    contratistaId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePathCuentas + 'cuentascobro.list.html',
+                        controller: 'contratistaCtrl',
+                        controllerAs: 'ctrl'
+                    },
+                    'detailView': {
+                        templateUrl: basePath + 'contratistas.detail.html',
                         controller: 'contratistaCtrl',
                         controllerAs: 'ctrl'
                     }
